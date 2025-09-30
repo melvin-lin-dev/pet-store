31b3074f593498f3331c46a608e55f69ced5d5ab
Melvin Lin <melvin.lin.work@gmail.com>
2025-09-30 08:16:03 +0700

    feat(pet-management/list): add pet list page with DataTable and simple sample data

---
+++ lib/features/pet_management/presentation/pages/pet_list_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class PetListPage extends StatelessWidget {
+  const PetListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List pets = [
+      {
+        "name": "Pet 1",
+        "category": {"id": 1, "name": "Dogs"},
+        "tags": [
+          {"id": 0, "name": "string"},
+        ],
+        "status": "available",
+      },
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Pet List')),
+      body: DataTable(
+        columns: const [
+          DataColumn(label: Text('Name')),
+          DataColumn(label: Text('Category')),
+          DataColumn(label: Text('Tags')),
+          DataColumn(label: Text('Status')),
+          DataColumn(label: Text('Action')),
+        ],
+        rows: pets
+            .map(
+              (pet) => DataRow(
+                cells: [
+                  DataCell(Text(pet['name'])),
+                  DataCell(Text(pet['category']['name'])),
+                  DataCell(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: (pet['tags'] as List<dynamic>)
+                          .map((tag) => Text(tag['name']))
+                          .toList(),
+                    ),
+                  ),
+                  DataCell(Text(pet['status'])),
+                  DataCell(
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            print('edit');
+                          },
+                          icon: Icon(Icons.edit, color: Colors.yellow),
+                          tooltip: 'Edit',
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            print("delete");
+                          },
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          tooltip: 'Delete',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
